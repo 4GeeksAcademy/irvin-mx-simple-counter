a41fefd4d18e6d30dcc96d890b6899a3a2fea428
Irvin Martinez <100785904+Irvin-Mx@users.noreply.github.com>
2025-04-03 05:44:26 +0000

    Modify Counter.jsx styles

---
+++ src/js/components/Counter.jsx
@@ -0,0 +1,35 @@
+import React , {useState, useEffect} from"react"
+
+//Counter styles
+const Counter = () => {
+
+    //Component state
+    const [counter,setCounter] = useState(['0','0','0','0'])
+    const [counterSeconds,setCounterSeconds] = useState(0)
+
+    //React hooks
+    useEffect(()=> {
+        setInterval(()=>{
+           setCounterSeconds(prev => prev +1)
+        },1000)
+    },[])
+
+     useEffect(()=> {
+        console.log(counterSeconds)
+        setCounter(   counterSeconds.toString().padStart(4, "0").split("") )
+             
+     },[counterSeconds])
+
+    return (
+        <div className="bg-black bg-gradient d-flex justify-content-around align-items-center col-9 p-2 rounded">
+            <div><i className="fa-solid fa-clock display-1 text-white"></i></div>
+            <div className="text-center border border-2 display-1 fw-semibold  bg-dark bg-gradient col-2 rounded text-white">{counter[0]}</div>
+            <div className="text-center border border-2  display-1 fw-semibold  bg-dark bg-gradient col-2 rounded text-white">{counter[1]}</div>
+            <div className="text-center border border-2 display-1 fw-semibold  bg-dark bg-gradient col-2 rounded text-white">{counter[2]}</div>
+            <div className="text-center border border-2 display-1 fw-semibold bg-dark bg-gradient col-2 rounded text-white">{counter[3]}</div>
+            <button  onClick={()=>setCounterSeconds(0)} type="button" class="btn btn-light">Reset</button>
+        </div>
+    )
+}
+
+export default Counter
